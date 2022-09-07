f11ef2dededd85b325a41622a5f634470de3da4e
Lucas Pickering <lucas@lucaspickering.me>
2022-09-07 14:37:19 -0400

    Add eslint configs

---
+++ packages/eslint-config/README.md
@@ -0,0 +1,8 @@
+# @lucaspickering/eslint-config
+
+My personal ESlint config for TypeScript projects. There are multiple configs available:
+
+- `@lucaspickering` - Base TypeScript config
+- `@lucaspickering/eslint-config/react` - TypeScript+React config
+
+The configs are combined into one package to make it easier to bump versions. This does mean that you could potentially be pulling in unnecessary dependencies for configs you aren't using, but that tradeoff is worth it for me right now since 95% of my projects are TypeScript+React.
---
+++ packages/eslint-config/react.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  extends: [
+    "@lucaspickering",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  settings: {
+    react: {
+      pragma: "React",
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/prop-types": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+    "react/display-name": "error",
+    "jsx-a11y/no-autofocus": "off",
+  },
+};
